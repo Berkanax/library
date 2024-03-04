fcfaced6d6de04905b38b24f2f9d4b8c6edbd46a
Adrian <adrian.mosk@outlook.com>
2024-03-04 20:10:06 +0100

    Initial commit

---
+++ src/Library.java
@@ -0,0 +1,21 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.1";
+
+        String title = "Zbuntowana";
+        String author = "Veronica Roth";
+        int releaseDate = 2012;
+        int pages = 367;
+        String publisher = "Amber";
+        String isbn = "9788324153497";
+
+        System.out.println(appName);
+        System.out.println("Książki dostepnę w bibliotece:");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
+}
